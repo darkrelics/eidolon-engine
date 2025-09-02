21e904baebc6f36580480e45ddc8cf54f819eb94
Jason E. Robinson <jason.robinson@auspex-labs.com>
2025-09-02 04:14:38 -0400

    WIP

--- incremental/lib/screens/game_screen.dart
+++ incremental/lib/screens/game_screen.dart
@@ -204,7 +204,7 @@ class _GameScreenState extends State<GameScreen> {
       } catch (e) {
         debugPrint('GameScreen: Error in polling loop: $e');
         // Continue polling after a delay
-        await Future.delayed(const Duration(seconds: 5));
+        await Future.delayed(const Duration(seconds: 30));
       }
     }
     
