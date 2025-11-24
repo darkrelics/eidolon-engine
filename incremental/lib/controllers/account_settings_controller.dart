9a5e6b69c0544360f04c9681b70a5d08b27d1fe5
Jason E. Robinson <jason.robinson@auspex-labs.com>
2025-11-23 19:00:03 -0500

    Refactor Account Setting Screen

---
+++ incremental/lib/controllers/account_settings_controller.dart
@@ -0,0 +1,40 @@
+import 'package:eidolon_incremental/providers/auth_provider.dart';
+import 'package:flutter/foundation.dart';
+
+class AccountSettingsController extends ChangeNotifier {
+  final AuthProvider _authProvider;
+  bool _isLoading = false;
+
+  bool get isLoading => _isLoading;
+
+  AccountSettingsController({required AuthProvider authProvider}) : _authProvider = authProvider;
+
+  Future<void> signOut({required VoidCallback onSuccess, required Function(dynamic) onError}) async {
+    _setLoading(true);
+    try {
+      await _authProvider.signOut();
+      onSuccess();
+    } catch (e) {
+      onError(e);
+    } finally {
+      _setLoading(false);
+    }
+  }
+
+  Future<void> deleteAccount({required VoidCallback onSuccess, required Function(dynamic) onError}) async {
+    _setLoading(true);
+    try {
+      await _authProvider.deleteAccount();
+      onSuccess();
+    } catch (e) {
+      onError(e);
+    } finally {
+      _setLoading(false);
+    }
+  }
+
+  void _setLoading(bool value) {
+    _isLoading = value;
+    notifyListeners();
+  }
+}
